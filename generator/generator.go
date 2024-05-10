c1f05f2d385dd3a7d2b4854e041d06a7454e6fb5
Chheung <chienchheung@gmail.com>
2024-05-10 20:38:21 +0700

    update

--- generator/generator.go
+++ generator/generator.go
@@ -9,9 +9,9 @@ import (
 )
 
 type TronWallet struct {
-	PrivateKey    string `json:"private_key"`
-	PublicKey     string `json:"public_key"`
-	Address       string `json:"address"`
+	PrivateKey string `json:"private_key"`
+	PublicKey  string `json:"public_key"`
+	Address    string `json:"address"`
 }
 
 func GenerateWallet() (TronWallet, error) {
